5847d8b6eeaced595be6bebb8ddabcc7890edddf
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2020-03-12 23:20:42 -0400

    First deploy (#14)
    * Add code of conduct
    
    * Improve code of conduct and implement navbar
    
    * Fix own observations
    
    * Fix text in code of conduct
    
    * Add metatags
    
    * Add google analytics
    
    * Change meetup link
    
    * Changed some prefixes to iwd.
    
    * Updated the CircleCI status badge.
    
    * Updated to last Angular 8 version.
    
    * Updated to Angular 9.
    
    * Fixed some update errors.
    
    * Update WTM about content
    
    * Update GDG about content
    
    * Change partner content
    
    * Updated site metadata and removed devfest analytics.
    
    Co-authored-by: mariagarciaflores <grissgarcia74@gmail.com>
    Co-authored-by: Brian Dennis Vega Hidalgo <Dentvega6@gmail.com>

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('gdg-website app is running!');
+    expect(page.getTitleText()).toEqual('iwd-website app is running!');
   });
 
   afterEach(async () => {
